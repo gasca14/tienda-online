b5e271b9bdc930c87ae312593221ffdeef7eae1c
gasca14 <alexgasca89@gmail.com>
2022-12-27 16:27:40 -0600

    agregando titulo a la pagina, agregando icono de marca en navbar,acomodando el login

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import '../Navbar/Navbar.css'
+import logo from '../../assets/images/perfume.png'
 function Navbar() {
     return (
         <>
             <nav id='nav' className="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Fifth navbar example">
                 <div className="container-fluid">
-                    <NavLink className="nav-link" to="/home">The parfum</NavLink>
+                    {/* <NavLink className="nav-link" to="/home">The parfum</NavLink> */}
+                    <Link className="navbar-brand" to="/home">
+                        <img id='log' src={logo} alt="img" />
+                    </Link>
                     <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon" />
                     </button>
