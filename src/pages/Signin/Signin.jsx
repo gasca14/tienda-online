0921d7d25981d8abee1ee9ee54753af8e57c3827
gasca14 <alexgasca89@gmail.com>
2023-01-08 23:10:49 -0600

    cambios en signin y signup poniendo en componentes separados

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
-import { loginService, registerService } from '../../services/Auth';
+import { loginService } from '../../services/Auth';
 import './Signin.css'
 import img from '../../assets/images/man.jpg'
+import { Link } from 'react-router-dom';
 
 
 const Signin = () => {
     const [typeForm, setTypeForm] = useState('signin');
     const [form, setForm] = useState({
-        nombre: '',
-        apellido: '',
-        edad: '',
         email: '',
         password: ''
     });
@@ -33,21 +31,8 @@ const Signin = () => {
                     console.log(error);
                 })
         }
-        else {
-
-            registerService(form)
-                .then(response => {
-                    console.log(response);
-                })
-                .catch(error => {
-                    console.log(error);
-                })
-        }
     }
 
-    const showSignIn = () => {
-        setTypeForm('signin');
-    };
 
     const showSignUp = () => {
         setTypeForm('signup');
@@ -63,28 +48,10 @@ const Signin = () => {
                 <div className="bg-white p-5 rounded-5 text-success shadow " style={{ width: '25rem' }}>
 
                     <div className='text-center fs-1 fw-bold text-coffe'>
-                        <h1>{typeForm === 'signin' ? 'Sign In' : 'Sign Up'}</h1>
+                        <h1>Sign In</h1>
                     </div>
                     <form onSubmit={saveData}>
-                        {typeForm === 'signup' &&
-                            <>
-                                <div className="mb-3">
-                                    <label htmlFor="nombre" className="form-label">Nombre</label>
-                                    <input type="text" className="form-control" name="nombre" placeholder='Ingresa tu nombre' required onChange={handleInputChange} />
-                                </div>
-
-                                <div className="mb-3">
-                                    <label htmlFor="apellido" className="form-label">Apellido</label>
-                                    <input type="text" className="form-control" name="apellido" placeholder='Ingresa tu apellido' required onChange={handleInputChange} />
-                                </div>
-
-                                <div className="mb-3">
-                                    <label htmlFor="edad" className="form-label">Edad</label>
-                                    <input type="number" className="form-control" name="edad" placeholder='Ingresa tu edad' required onChange={handleInputChange} />
-                                </div>
-                            </>
-                        }
-
+                                
                         <div className="mb-3">
                             <label htmlFor="email" className="form-label">Email</label>
                             <input type="email" className="form-control" name="email" placeholder='Ingresa tu email' required onChange={handleInputChange} />
@@ -103,16 +70,10 @@ const Signin = () => {
                     {typeForm === 'signin' &&
                         <div className="d-flex gap-1 justify-content-center mt-1">
                             <div>¿No tienes cuenta?</div>
-                            <p className="signup" onClick={showSignUp}>Sign Up</p>
+                            <Link to='/signup' className="signup" onClick={showSignUp}>Sign Up</Link>
                         </div>
                     }
 
-                    {typeForm === 'signup' &&
-                        <div className="d-flex gap-1 justify-content-center mt-1">
-                            <div>¿Ya tienes cuenta?</div>
-                            <p className="signup" onClick={showSignIn}>Sign In</p>
-                        </div>
-                    }
                 </div>
             </div>
         </>
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,9 +1,164 @@
-function Signup() {
+import React, { useState } from 'react';
+import { registerService } from '../../services/Auth';
+// import './Signin.css'
+import img from '../../assets/images/man.jpg'
+import { Link } from 'react-router-dom';
+
+
+const Signup = () => {
+    const [typeForm, setTypeForm] = useState('signup');
+    const [form, setForm] = useState({
+        nombre: '',
+        apellido: '',
+        edad: '',
+        email: '',
+        password: ''
+    });
+
+    const handleInputChange = (event) => {
+        setForm({
+            ...form,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const saveData = (event) => {
+        event.preventDefault();
+        // console.log(form);
+        if (typeForm === 'signup') {
+            registerService(form)
+                .then(response => {
+                    console.log(response);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        }
+    }
+
+    const showSignIn = () => {
+        setTypeForm('signin');
+    };
+
+   
+
     return (
         <>
-            <h1>aqui deberia ir el singup</h1>
+            <div>
+                <img id='img' src={img} alt="portada" />
+            </div>
+            <div className="mt-5 d-flex justify-content-center align-items-center vh-90">
+                <div className="bg-white p-5 rounded-5 text-success shadow " style={{ width: '25rem' }}>
+
+                    <div className='text-center fs-1 fw-bold text-coffe'>
+                        <h1>Sign Up</h1>
+                    </div>
+                    <form onSubmit={saveData}>
+                    <div className="mb-3">
+                                   <label htmlFor="nombre" className="form-label">Nombre</label>
+                                     <input type="text" className="form-control" name="nombre" placeholder='Ingresa tu nombre' required onChange={handleInputChange} />
+                                 </div>
+
+                                 <div className="mb-3">
+                                     <label htmlFor="apellido" className="form-label">Apellido</label>
+                                     <input type="text" className="form-control" name="apellido" placeholder='Ingresa tu apellido' required onChange={handleInputChange} />
+                                 </div>
+
+                                 <div className="mb-3">
+                                     <label htmlFor="edad" className="form-label">Edad</label>
+                                     <input type="number" className="form-control" name="edad" placeholder='Ingresa tu edad' required onChange={handleInputChange} />
+                                 </div>
+
+                             
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input type="email" className="form-control" name="email" placeholder='Ingresa tu email' required onChange={handleInputChange} />
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input type="password" className="form-control" name="password" placeholder='Ingresa tu password' required onChange={handleInputChange} />
+                        </div>
+                        <button
+                            type='submit'
+                            className='btn btn-success w-100 mt-4 fw-semibold shadow-sm'>
+                            {typeForm === 'signin' ? 'Sign In' : 'Sign Up'}
+                        </button>
+                    </form>
+
+                    {typeForm === 'signup' &&
+                        <div className="d-flex gap-1 justify-content-center mt-1">
+                            <div>¿Ya tienes cuenta?</div>
+                            <Link to='/signin' className="signup" onClick={showSignIn}>Sign In</Link>
+                        </div>
+                    }
+                </div>
+            </div>
         </>
     );
 }
 
 export default Signup;
+
+
+//     return (
+//         <>
+//             <div>
+//                 <img id='img' src={img} alt="portada" />
+//             </div>
+//             <div className="mt-5 d-flex justify-content-center align-items-center vh-90">
+//                 <div className="bg-white p-5 rounded-5 text-success shadow " style={{ width: '25rem' }}>
+
+//                     <div className='text-center fs-1 fw-bold text-coffe'>
+//                         <h1>Sign Up</h1>
+//                     </div>
+//                     <form onSubmit={saveData}>
+                        
+//                                 <div className="mb-3">
+//                                     <label htmlFor="nombre" className="form-label">Nombre</label>
+//                                     <input type="text" className="form-control" name="nombre" placeholder='Ingresa tu nombre' required onChange={handleInputChange} />
+//                                 </div>
+
+//                                 <div className="mb-3">
+//                                     <label htmlFor="apellido" className="form-label">Apellido</label>
+//                                     <input type="text" className="form-control" name="apellido" placeholder='Ingresa tu apellido' required onChange={handleInputChange} />
+//                                 </div>
+
+//                                 <div className="mb-3">
+//                                     <label htmlFor="edad" className="form-label">Edad</label>
+//                                     <input type="number" className="form-control" name="edad" placeholder='Ingresa tu edad' required onChange={handleInputChange} />
+//                                 </div>
+
+//                         <div className="mb-3">
+//                             <label htmlFor="email" className="form-label">Email</label>
+//                             <input type="email" className="form-control" name="email" placeholder='Ingresa tu email' required onChange={handleInputChange} />
+//                         </div>
+
+//                         <div className="mb-3">
+//                             <label htmlFor="password" className="form-label">Password</label>
+//                             <input type="password" className="form-control" name="password" placeholder='Ingresa tu password' required onChange={handleInputChange} />
+//                         </div>
+//                         <button
+//                             type='submit'
+//                             className='btn btn-success w-100 mt-4 fw-semibold shadow-sm'>
+//                             {typeForm === 'signin' ? 'Sign In' : 'Sign Up'}
+//                         </button>
+//                     </form>
+//                     {typeForm === 'signin' &&
+//                         <div className="d-flex gap-1 justify-content-center mt-1">
+//                             <div>¿No tienes cuenta?</div>
+//                             <p className="signup" onClick={showSignUp}>Sign Up</p>
+//                         </div>
+//                     }
+
+//                     {typeForm === 'signup' &&
+//                         <div className="d-flex gap-1 justify-content-center mt-1">
+//                             <div>¿Ya tienes cuenta?</div>
+//                             <Link  className="signup" onClick={showSignIn}>Sign In</Link>
+//                         </div>
+//                     }
+//                 </div>
+//             </div>
+//         </>
+//     );
+// export default Signup;
